9aadfbe1d00954d1730f8c000a516b87e3346800
wardha-ian1993 <favianwardhana1993@gmail.com>
2024-05-03 18:40:13 +0700

    finalizing

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const shortid = require('shortid');
 const app = express();
 
 // Basic Configuration
@@ -18,6 +22,41 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// --------------------------------------------------------------------------------
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: String
+});
+const Url = mongoose.model('Url', urlSchema);
+
+app.post('/api/shorturl', async (req, res) => {
+  const { original_url } = req.body;
+  const short_url = shortid.generate();
+
+  await Url.create({ original_url, short_url });
+
+  res.json({ original_url, short_url });
+});
+
+app.get('/api/shorturl/:short_url', async (req, res) => {
+  const { short_url } = req.params;
+  const url = await Url.findOne({ short_url });
+
+  if (!url) {
+    return res.status(404).json({ error: 'Short URL not found' });
+  }
+  res.redirect(url.original_url);
+});
+// --------------------------------------------------------------------------------
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
