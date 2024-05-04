f87ef399cf4466cd26aa52a0ea3b8ee663b4533d
wardha-ian1993 <favianwardhana1993@gmail.com>
2024-05-04 08:57:17 +0700

    finalizing

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const urlParser = require('url');
 const shortid = require('shortid');
+const dns = require('dns');
+
+const mongoose = require('mongoose');
 const app = express();
 
 // Basic Configuration
@@ -30,50 +33,37 @@ mongoose.connect("mongodb+srv://favianwardhana1993:2NE1uGidwJEYyUsS@fcc-moongose
 });
 
 const urlSchema = new mongoose.Schema({
-  original_url: String,
+  url: String,
   short_url: String
 });
 const Url = mongoose.model('Url', urlSchema);
 
-const isValidUrl = (str) => {
-  const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-  return !!pattern.test(str);
-};
-
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/api/shorturl', async (req, res) => {
-  const { original_url } = req.body;
-
-  if (!isValidUrl(original_url)) {
-    return res.status(400).json({ error: 'Invalid URL' });
-  }
-
+  const { url } = req.body;
   const short_url = shortid.generate();
-  
-  await Url.create({ original_url, short_url });
 
-  res.json({
-    "original_url": original_url,
-    "short_url": short_url
-  });
+  const isValidUrl = dns.lookup(urlParser.parse(url).hostname, (err, address) => {
+    if (!address) {
+      res.json({ error: "Invalid URL" })
+    } else {
+      Url.create({ url: url, short_url: short_url });
+      res.json({ "original_url": url, "short_url": short_url });
+    }
+  })
 });
 
 app.get('/api/shorturl/:short_url', async (req, res) => {
   const { short_url } = req.params;
-  const url = await Url.findOne({ short_url });
+  const url = await Url.findOne({ short_url: short_url });
 
   if (!url) {
     return res.status(404).json({ error: 'Short URL not found' });
   }
 
-  res.json({ original_url: url.original_url });
+  res.redirect(url.url);
 });
 // --------------------------------------------------------------------------------
 
